eb4f50e04947aff331edd85d0477c04cf47e8936
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2018-11-05 11:47:58 +0100

    exercice 2.1, done

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -13,18 +13,18 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+        alert(parseInt(document.getElementById("op-one").value) + parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        alert(parseInt(document.getElementById("op-one").value) - parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        alert(parseInt(document.getElementById("op-one").value) * parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+        alert(parseInt(document.getElementById("op-one").value) / parseInt(document.getElementById("op-two").value));
     });
 })();
