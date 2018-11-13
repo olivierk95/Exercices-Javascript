571bca3f6d9d0e8ee09a5330f78bd630a5b8cc94
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2018-11-13 10:24:03 +0100

    Exercice 6.12, done

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -11,6 +11,30 @@
 
 (function() {
 
-    // your code here
+    document.querySelector("#pass-one").addEventListener("input", () => {
+    	
+    	var password = document.querySelector("#pass-one").value;
+
+    	var character = password.split("");
+
+    	var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+    	var count = 0;
+
+    	for (var j=0; j<numbers.length; j++) {
+    		for (var i=0; i<character.length; i++) {
+    			if (character[i]===numbers[j]) {
+    				count++;
+    			};
+    		};
+    	};
+
+    	if (password.length > 7 && count > 1) {
+    		document.querySelector("#validity").innerHTML = "ok";
+    	} else {
+    		document.querySelector("#validity").innerHTML = "Pas ok";
+    	};
+    	
+    });
 
 })();
