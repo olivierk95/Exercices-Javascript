da3635419247d7149f3540e9ce290e2a17531c6b
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2018-11-14 12:28:18 +0100

    Exercice 7.6, done

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -11,6 +11,26 @@
 
 (function() {
 
-    // your code here
+    var tryToFind = Math.floor(Math.random()*(100-1))+1;
+
+    console.log(tryToFind);
+
+    var proposition = prompt("Essaie de deviner le nombre entre 1 et 100!");
+
+    while (parseInt(proposition) !== tryToFind) {
+
+	    if (parseInt(proposition) > tryToFind && 0<parseInt(proposition)<101) {
+	    	var proposition = prompt("Plus petit, try again");
+	    } else if (parseInt(proposition) < tryToFind && 0<parseInt(proposition)<101) {
+	    	var proposition = prompt("Plus grand, try again");
+	    } else {
+	    	var proposition = prompt("Merci de bien vouloir rentrer un nombre entre 1 et 100!")
+	    };
+
+	};
+
+	if (parseInt(proposition) == tryToFind) {
+	    alert("Bravo, vous êtes un winner!");
+	};
 
 })();
