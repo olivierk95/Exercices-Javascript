191d04893ff2f51e4d73bad8c9e29fbfa729f07a
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2018-11-15 16:19:28 +0100

    Exercice 7.1, done

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -11,34 +11,74 @@
 
 (function() {
 
-    var text = document.querySelector("#target").innerHTML;   
-    var character = text.split(""); //tous les caractères sont mis dans un tableau
+    var stringText = document.querySelector("#target").innerHTML;   
+    var textArray = stringText.split("");
 
-    var timeline = new TimelineMax({
-        repeat : 1,
-        repeatDelay : 0.4,
-        yoyo : true
-    });
- 
-    text.html("");
- 
-    $.each(character, function(index, val) {
-        if (val == " ") {
-            val = " ";
+    function wrap (item) {
+        return "<span>" + item + "</span>";
+    };
+
+    wrappedArray = textArray.map(wrap);
+
+    var newString = wrappedArray.join("");
+
+    document.querySelector("#target").innerHTML = newString;
+
+    for (var i = 1 ; i < stringText.length ; i = i +5) {
+        document.getElementsByTagName('span')[i].style.fontSize = "15px";
+    };
+
+    for (var i = 2 ; i < stringText.length ; i = i +5) {
+        document.getElementsByTagName('span')[i].style.fontSize = "25px";
+    };
+
+    for (var i = 3 ; i < stringText.length ; i = i +5) {
+        document.getElementsByTagName('span')[i].style.fontSize = "40px";
+    };
+
+    for (var i = 4 ; i < stringText.length ; i = i +5) {
+        document.getElementsByTagName('span')[i].style.fontSize = "30px";
+    };
+
+    for (var i = 5 ; i < stringText.length ; i = i +5) {
+        document.getElementsByTagName('span')[i].style.fontSize = "20px";
+    };
+
+
+    //Pour faire un effet dynamique de vague:
+    /*let i = 0;
+
+    function moveText() {
+
+        i++
+
+        document.getElementsByTagName('span')[i].style.fontSize = "25px";
+        document.getElementsByTagName('span')[i-1].style.fontSize = "35px";
+        if (i > 2 ) {document.getElementsByTagName('span')[i-2].style.fontSize = "45px";}
+        if (i > 3 ) {document.getElementsByTagName('span')[i-3].style.fontSize = "55px";}
+        if (i > 4 ) {document.getElementsByTagName('span')[i-4].style.fontSize = "65px";}
+        if (i > 5 ) {document.getElementsByTagName('span')[i-5].style.fontSize = "75px";}
+        if (i > 6 ) {document.getElementsByTagName('span')[i-6].style.fontSize = "65px";}
+        if (i > 7 ) {document.getElementsByTagName('span')[i-7].style.fontSize = "55px";}        
+        if (i > 8 ) {document.getElementsByTagName('span')[i-8].style.fontSize = "45px";}
+        if (i > 9 ) {document.getElementsByTagName('span')[i-9].style.fontSize = "35px";}
+        if (i > 10 ) {document.getElementsByTagName('span')[i-10].style.fontSize = "25px";}        
+
+
+
+        if (i < textArray.length) {
+        
+            setTimeout (function () {
+            
+                moveText();
+
+            }, 150);
         }
-        var letter = $("", {
-            id : "txt" + index
-        }).html(val).appendTo(text);
-        timeline.to(letter, 1, {
-            rotationY : 360,
-            rotationX : 360,
-            textShadow : "0px 0px 0px white",
-            color : "none",
-            autoAlpha : 1,
-            ease : Back.easeOut,
-            transformOrigin : "50% 50% -20"
-        }, index * 0.05);
-    });
 
 
+        
+    }
+
+    moveText();*/
+    
 })();
